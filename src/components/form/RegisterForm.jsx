fa46c0579b63f4c743cc5991071013a11ed2f62e
caiomcs04 <caio6c@yahoo.com.br>
2021-06-04 11:20:23 -0300

    useState aplicatiomn

--- src/components/form/RegisterForm.jsx
+++ src/components/form/RegisterForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from "@material-ui/core/TextField"
 import Switch from "@material-ui/core/Switch"
@@ -6,14 +6,66 @@ import FormControlLabel from "@material-ui/core/FormControlLabel"
 
 
 function RegisterForm() {
+  const [name, setName] = useState("");
+  const [surName, setSurname] = useState("");
+
   return (
-    <form>
-      <TextField margin="normal" variant="outlined" id="name" fullWidth label="Nome"></TextField>
-      <TextField margin="normal" variant="outlined" id="surname" fullWidth label="Sobrenome"></TextField>
-      <TextField margin="normal" variant="outlined" id="cpf" fullWidth label="CPF"></TextField>
-      <FormControlLabel label="Promoções" control={<Switch margin="normal" id="promotion" defaultChecked color="primary" name="Promoções"/>}/>
-      <FormControlLabel label="Novidades" control={<Switch margin="normal" id="news" defaultChecked color="primary" name="Novidades"/>}/>
-      <Button variant="contained" color="primary">Cadastrar</Button>
+    <form onSubmit={(event) => {
+      event.preventDefault();
+      console.log(name)
+    }}>
+      <TextField
+      value={name}
+        onChange={(event) => {
+          setName(event.target.value)
+        }}
+        margin="normal"
+        variant="outlined"
+        id="name"
+        fullWidth
+        label="Nome"
+      />
+      <TextField
+          value={surName}
+          onChange={(event) => {
+            setSurname(event.target.value)
+          }}
+        margin="normal"
+        variant="outlined"
+        id="surname"
+        fullWidth
+        label="Sobrenome"
+      />
+      <TextField
+        margin="normal"
+        variant="outlined"
+        id="cpf"
+        fullWidth
+        label="CPF"
+      />
+      <FormControlLabel
+        label="Promoções"
+        control={<Switch margin="normal"
+          id="promotion"
+          defaultChecked
+          color="primary"
+          name="Promoções" />}
+      />
+      <FormControlLabel
+        label="Novidades"
+        control={<Switch
+          margin="normal"
+          id="news"
+          defaultChecked
+          color="primary"
+          name="Novidades" />}
+      />
+      <Button
+        type="submit"
+        variant="contained"
+        color="primary">
+        Cadastrar
+      </Button>
     </form>
   )
 }
